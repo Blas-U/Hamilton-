903c9f7fad592a20338d90d802b29794978ba92e
Blas-U <cheezypizzagamer@gmail.com>
2021-04-20 10:20:44 -0500

    Added name

--- PlayStyle.py
+++ PlayStyle.py
@@ -1,7 +1,28 @@
 with open ("Satisfied.txt") as f:
     s = f.readlines()
 
-print (s[0])
+print (s)
 
-for i in range (0,60):
-    print (s[i].strip())
+totalwords = 0
+
+for i in s:
+    n = len(i.strip().split())
+    print (n)
+    totalwords = totalwords + n
+    print (totalwords)
+    print (len(s))
+nlines = len(s)
+aveWords = totalwords / nlines
+print (aveWords)
+
+
+
+
+##for i in range (len(s)):
+##    print (s[i].strip())
+##
+##nlines = len(s)
+##
+##print (f'the number of lines is {nlines}')
+##line = s[].split()
+##print (line)
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Hamilton-
+Project
